5fb1837e8350f46be8fcd61debf08e8c38e5a65b
Thirdareddy <72080540+Thirdareddy@users.noreply.github.com>
2020-10-28 18:24:15 +0530

    Add files via upload

---
+++ zombiedemo.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<unistd.h>
+int main()
+{
+pid_t id;
+id=fork();
+
+if(0==id)
+{
+printf("child starting\n");
+sleep(2);
+printf("exiting child\n");
+}
+else
+{
+printf("parent starting\n");
+sleep(12);
+printf("exiting parent\n");
+}
+}
